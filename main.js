30cece8534764b37137c797c634c93bf55d0875a
Sam Mudick <smudick@gmail.com>
2020-07-20 18:51:58 -0500

    tested data and primitive types

--- main.js
+++ main.js
@@ -1,1 +1,39 @@
-console.log("Loggin it!");
+///////// undefined ///////////
+
+
+////////// boolean ////////////
+
+
+////////// Number ////////////
+
+
+////////// String ////////////
+
+
+// let array = [];
+
+// let name = 'Dr. T',
+//     age = "secret",
+//     school = "NSS";
+
+// let object = {
+//     name,
+//     age,
+//     school,
+// };
+
+// console.log(object.name);
+
+
+// //es5
+// let myFunction = () => {
+//     return
+// }
+
+// //es6
+// function thatFunction() {
+//     return
+// }
+
+// console.log(typeof thatFunction);
+
